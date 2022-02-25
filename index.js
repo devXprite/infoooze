b4d9f086e98d6a32dd8a4fa69803d8073776b235
7ORP3DO <davisudha806@gmail.com>
2022-02-25 12:26:56 +0530

    add prettier

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import chalk from 'chalk';
 import colors from 'colors';
 import chalkAnimation from "chalk-animation";
 
-import {banner } from './src/banner.js';
+import { banner } from './src/banner.js';
 
 const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
 
