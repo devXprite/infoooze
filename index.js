021874b02d35e3446c46e04af6382d29c100cc82
7ORP3DO <davisudha806@gmail.com>
2022-02-25 15:34:22 +0530

    add mailfinder

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import chalk from 'chalk';
 import colors from 'colors';
-import chalkAnimation from "chalk-animation";
+import chalkAnimation from 'chalk-animation';
 
 import { banner } from './src/banner.js';
 
 const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
 
 async function welcome() {
-    const glitchTitle = chalkAnimation.glitch(
-        'Welcome to Dhanush!\n'
-    );
-    // await sleep(3000);
-    glitchTitle.stop();
+  const glitchTitle = chalkAnimation.glitch('Welcome to Dhanush!\n');
+  await sleep(3000);
+  glitchTitle.stop();
 }
 
 await welcome();
 await banner();
+
+// import { tmp } from './src/tmp.js';
+// tmp();
--- src/banner.js
+++ src/banner.js
@@ -1,61 +1,75 @@
-import chalk from "chalk";
-import chalkAnimation from "chalk-animation";
-import PromptSync from "prompt-sync";
+import chalk from 'chalk';
+import chalkAnimation from 'chalk-animation';
+import PromptSync from 'prompt-sync';
 
-import userrecon from "./userrecon.js";
+import userrecon from './userrecon.js';
+import mailfinder from './mailfinder.js';
 
 var i = 1;
 const prompt = PromptSync();
 const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
-const list = async (name, description) => { await sleep(30); console.log(`\t\t ${chalk.cyan(i++)} ${chalk.whiteBright(name)} \t ${chalk.blackBright(description)}`); }
+const list = async (name, description) => {
+  await sleep(300);
+  console.log(
+    ` ${chalk.cyan(i++)} ${chalk.whiteBright(name)} \t ${chalk.blackBright(
+      description,
+    )}`,
+  );
+};
 
 const takeOption = () => {
-    var selectedOption = prompt(`\t\t ${'>'.cyan} ${'Your Option'.bold} : `);
-    switch (selectedOption) {
-        case '1':
-            userrecon()
-            break;
+  var selectedOption = prompt(` ${'>'.cyan} ${'Your Option'.bold} : `);
+  switch (selectedOption) {
+    case '1':
+      userrecon();
+      break;
+    case '2':
+      mailfinder();
+      break;
 
-        default:
-            takeOption();
-            break;
-    }
-}
+    case 'exit':
+      console.log('bye!'.cyan);
+      break;
+
+    default:
+      takeOption();
+      break;
+  }
+};
 
 export async function banner() {
-    console.clear();
-    chalkAnimation.neon(`
-\t\t       |.---------------.|
-\t\t       ||               ||
-\t\t       ||   -._ .-.     ||
-\t\t       ||   -._| | |    ||
-\t\t       ||   -._|"|"|    ||
-\t\t       ||   -._|.-.|    ||
-\t\t       ||_______________||
-\t\t       /.-.-.-.-.-.-.-.-.\\
-\t\t      /.-.-.-.-.-.-.-.-.-.\\
-\t\t     /.-.-.-.-.-.-.-.-.-.-.\\
-\t\t    /______/__________\\___o_\\ 
-\t\t    \\_______________________/
-    
-    `, '2');
+  console.clear();
+  chalkAnimation.neon(
+    `
+         |.---------------.|
+         ||               ||
+         ||   -._ .-.     ||
+         ||   -._| | |    ||
+         ||   -._|"|"|    ||
+         ||   -._|.-.|    ||
+         ||_______________||
+         /.-.-.-.-.-.-.-.-.\\
+        /.-.-.-.-.-.-.-.-.-.\\
+       /.-.-.-.-.-.-.-.-.-.-.\\
+      /______/__________\\___o_\\ 
+      \\_______________________/
+      
+    `,
+    '2',
+  );
 
-    await sleep(200);
-    console.log(`\t\t A tool by ${'7ORP3DO'.cyan} & ${'Biswajeet'.cyan} `);
-    await sleep(100);
-    
-    console.log('\t     ' + ' Choose number or type exit fir exiting '.bgWhite.black + '\n\n');
-    await list("Userrexon", 'lorem text');
-    await list("Facedumper", 'Dump facebook information');
-    await list('Mailfinder', 'find email with name');
-    await list('Gogle Dork', 'Dorking with google search');
-    await list('Phoneinfo', 'Phone Number Information');
-    await list('Phoneinfo', 'Phone Number Information');
-    await list('Phoneinfo', 'Phone Number Information');
-    console.log('\n');
-    takeOption();
+  await sleep(2000);
+  console.log(`      A tool by ${'7ORP3DO'.cyan} & ${'Biswajeet'.cyan} `);
+  await sleep(1000);
+
+  console.log(
+    '' + ' Choose number or type exit fir exiting '.bgWhite.black + '\n\n',
+  );
+  await list('Userrecon', 'username reconnaissance');
+  await list('Mailfinder ', 'find email with specific name');
+
+  console.log('\n');
+  takeOption();
 }
-
-
 
 export default banner;
