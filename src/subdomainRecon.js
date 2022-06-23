3632ec4548967bf2592f0d07b7332e7316ccd6b0
DanielAzulay9 <azulay459@gmail.com>
2022-06-23 23:10:54 +0300

    the exported file, integrating amass as a tool.

---
+++ src/subdomainRecon.js
@@ -0,0 +1,22 @@
+const { spawn } = require('child_process');
+const { input, errorMsg, currentTimeStamp, info, goBack } = require('./helper');
+
+const subdomainRecon = async (host, showHome = false) => {
+  host = host || (await input('Enter your host (example.com):'));
+
+  const path = `${process.cwd()}/results/infoooze_subdomainRecon_${currentTimeStamp()}.txt`;
+  info(`Results will be saved in ${path}`);
+
+  // Passive subdomain scan:
+  const command = spawn('amass', ['enum', '-passive', '-d', host, '-o', path]);
+  command.stdout.on('data', (output) => {
+    console.log(output.toString());
+  });
+  command.on('error', (err) => {
+    errorMsg(
+      `amass might not be installed, try installing it: go install -v github.com/OWASP/Amass/v3/...@master`,
+    );
+  });
+};
+
+module.exports = subdomainRecon;
