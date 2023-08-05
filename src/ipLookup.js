ddbf533061e1754a2935496068c9afacedff779c
jayfort <102162436+j4t-git@users.noreply.github.com>
2023-08-06 01:14:42 +0200

    Update ipLookup.js
    More optimized looping

--- src/ipLookup.js
+++ src/ipLookup.js
@@ -10,7 +10,7 @@ const {
   exit,
 } = require('./helper');
 
-const IPlookup = async (ip, showHome = false, i = 1) => {
+const IPlookup = async (ip, showHome = false) => {
   ip = ip || (await input('Your IP', 'ip'));
   const path = `${process.cwd()}/results/infoooze_IPLookup_${currentTimeStamp()}.txt`;
   info('Results will be saved in ', path);
@@ -24,10 +24,10 @@ const IPlookup = async (ip, showHome = false, i = 1) => {
     });
     const IPData = response.data;
 
-    for (const key in IPData) {
-      list(i++, key, IPData[key]);
-      saveTo(path, key, IPData[key]);
-    }
+    Object.entries(IPData).forEach(([key, value], i) => {
+      list(i + 1, key, value);
+      saveTo(path, key, value);
+    });
 
     if (!showHome) exit();
   } catch (error) {
